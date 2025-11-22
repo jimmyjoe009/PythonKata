1215a7926a6cfc6b6077f0b458b6a6be7a8ca3cd
jimmyjoe009 <alfredo.jime@gmail.com>
2025-11-22 10:33:26 +0100

    Create maxSubStr.py

---
+++ maxSubStr.py
@@ -0,0 +1,21 @@
+# Find the Length of the Longest Substring Without Repeating Characters
+
+# Given a string s, return the length of the longest substring that has no repeating characters.
+
+def maxstr(longstr):
+    biggest=""
+    current=""
+    mymap=set()
+    str_len=0
+    for c in longstr:
+        if c not in mymap:
+            mymap.add(c)
+            current+=c
+            if len(current) > len(biggest):
+                biggest=current
+        else:
+            current=c
+            mymap=set()
+    return(biggest)
+
+print(maxstr("abcabcbbholygrail"))
