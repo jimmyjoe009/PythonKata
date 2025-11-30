6e0542c4a042a08794e509b7559662c43a4fd557
jimmyjoe009 <alfredo.jime@gmail.com>
2025-11-30 19:16:46 +0100

    Update ArrayDiff.py

--- ArrayDiff.py
+++ ArrayDiff.py
@@ -8,3 +8,14 @@ def array_diff(a, b):
         while (new_list.count(i) > 0):
             new_list.remove(i)
     return(new_list)
+
+
+# Improved version. Lower complexity
+def array_diff2(a,b):
+    the_set=set(b)
+    return[i for i in a if (i not in the_set)]
+
+list1=[1,2,3,4,5,6,7]
+list2=[5,6,7,8,9,10]
+print(array_diff(list1,list2))
+print(array_diff2(list1,list2))
